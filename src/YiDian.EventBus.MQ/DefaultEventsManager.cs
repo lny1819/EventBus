09037b4e4084250f22f1f29425788a63428235de
yanfe <yanfe@DESKTOP-T3NCBJR>
2019-06-30 10:56:54 +0800

    +

---
+++ src/YiDian.EventBus.MQ/DefaultEventsManager.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace YiDian.EventBus.MQ
+{
+    public class DefaultEventsManager : IAppEventsManager
+    {
+        static readonly CheckResult Success = new CheckResult() { IsVaild = true, InvaildMessage = "" };
+        public CheckResult GetEventId(string typename)
+        {
+            return new CheckResult() { IsVaild = true, InvaildMessage = typename };
+        }
+
+        public List<EventId> GetEventIds(string appname)
+        {
+            return new List<EventId>();
+        }
+
+        public CheckResult GetVersion(string appName)
+        {
+            return new CheckResult() { IsVaild = true, InvaildMessage = "0.0" };
+        }
+
+        public AppMetas ListEvents(string appName)
+        {
+            return new AppMetas() { Enums = new List<EnumMeta>(), MetaInfos = new List<ClassMeta>(), Name = appName, Version = "0.0" };
+        }
+
+        public CheckResult RegisterEvent<T>(string appName, string version) where T : IMQEvent
+        {
+            return Success;
+        }
+
+        public CheckResult VaildityTest(string appName, string version)
+        {
+            return Success;
+        }
+    }
+}

4017ca60523b194adfaef5eb8920a6a660fedf1c
xhero <yanfeng.sun@live.com>
2019-08-09 20:03:16 +0800

    +

--- test/ConsoleApp/StartUp.cs
+++ test/ConsoleApp/StartUp.cs
@@ -22,9 +22,66 @@ namespace ConsoleApp
         {
             for (; ; )
             {
-                DispatchAndChannels.Test(args);
+                //DispatchAndChannels.Test(args);
+                var configbuilder = new ConfigurationBuilder();
+                configbuilder.Add(new SA());
+                var config = configbuilder.Build();
+                var value = config["zs"];
+                DictTest.Test();
                 Console.ReadKey();
             }
+        }
+    }
+    class SBc : IChangeToken, IDisposable
+    {
+        public bool HasChanged { get; }
+
+        public bool ActiveChangeCallbacks { get; }
+
+        public void Dispose()
+        {
+
+        }
+
+        public IDisposable RegisterChangeCallback(Action<object> callback, object state)
+        {
+            return this;
+        }
+    }
+    class SA : IConfigurationSource
+    {
+        public IConfigurationProvider Build(IConfigurationBuilder builder)
+        {
+            return new SB();
+        }
+    }
+    class SB : IConfigurationProvider
+    {
+        public IEnumerable<string> GetChildKeys(IEnumerable<string> earlierKeys, string parentPath)
+        {
+            return new List<string>() { "zs", "ls" };
+        }
+
+        public IChangeToken GetReloadToken()
+        {
+            return new SBc();
+        }
+
+        public void Load()
+        {
+
+        }
+
+        public void Set(string key, string value)
+        {
+            Console.WriteLine("Set");
+        }
+
+        public bool TryGet(string key, out string value)
+        {
+            value = "test";
+            Console.WriteLine("TryGet");
+            return true;
         }
     }
 }

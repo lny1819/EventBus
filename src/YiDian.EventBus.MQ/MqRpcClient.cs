a7e4c1f75ead7d8a61b1e5f81237c26890045ae0
xhero <yanfeng.sun@Live.com>
2019-11-18 16:05:26 +0800

    3.0.0.2

--- src/YiDian.EventBus.MQ/MqRpcClient.cs
+++ src/YiDian.EventBus.MQ/MqRpcClient.cs
@@ -21,7 +21,7 @@ namespace YiDian.EventBus.MQ
         public bool IsConnect { get; set; }
         public ResponseBase<TOut> Call<TOut, Tin>(string uri, Tin data)
         {
-            var bytes = Encode.GetBytes(ToJson(data));
+            var bytes = Encode.GetBytes(data.ToJson());
             return Call<TOut>(uri, bytes);
         }
         static readonly DateTime start = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
@@ -29,20 +29,6 @@ namespace YiDian.EventBus.MQ
         public static long ToUnixTimestamp(DateTime dateTime)
         {
             return Convert.ToInt64((dateTime.ToUniversalTime() - start).TotalSeconds);
-        }
-        public string ToJson(object o)
-        {
-            if (o == null) return "";
-            string value;
-            var type = o.GetType();
-            if (type == typeof(String))
-            {
-                value = o.ToString();
-            }
-            else if (type.IsEnum) value = ((int)o).ToString();
-            else if (type.IsValueType) value = o.ToString();
-            else value = JsonSerializer.Serialize(o);
-            return value;
         }
         public ResponseBase<T> Call<T>(string uri)
         {
